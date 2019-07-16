29820d75550d02c7d8fed1293312a9ee56bf3058
jswens912 <jswens912@gmail.com>
2019-07-16 17:22:38 -0500

    Lab 07 constructor functions

---
+++ constructor.js
@@ -0,0 +1,129 @@
+'use strict';
+
+
+var hours = ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function CookieStore(location, minCust, maxCust, avgCookie){
+    this.location = location,
+    this.minCust = minCust,
+    this.maxCust = maxCust,
+    this.avgCookie = avgCookie,
+    
+    this.cookiesSold = [],
+    this.cookiePerHour();
+    //CookieStore.totalData.push(this);
+}
+
+
+ CookieStore.totalData= [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+CookieStore.prototype.customerPerHour = function(){
+    var customers= Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+    return(customers);
+};
+
+CookieStore.prototype.cookiePerHour = function(){
+    for(var i=0; i<hours.length; i++){
+     this.cookiesSold[i]=Math.floor(this.customerPerHour()*this.avgCookie);
+    }
+};
+
+
+//console.table(firstAndPike);
+
+
+function headerData() {
+    var thead = document.getElementById("table_head");
+    var td = document.createElement("th");
+    td.textContent = "Stores";
+    thead.appendChild(td);
+    for(var thIndex = 0; thIndex < hours.length; thIndex++){
+        var td = document.createElement("th");
+        td.textContent = hours[thIndex];
+        thead.appendChild(td);
+        }  
+        td = document.createElement("th");
+        td.textContent = "Daily Totals.";
+        thead.appendChild(td);
+    };
+
+function hourlyCookieData() {
+    var hourlyTotal = [];
+    for( var i=0; i<hours.length; i++){
+        hourlyTotal[i] = firstAndPike.cookiePerHour[i] + seaTac.cookiePerHour[i] + seattleCenter[i] +
+        capitolHill[i] + alki[i];
+    
+        return(hourlyTotal);
+    }
+}
+
+function footerData() {
+    var tFooter = document.getElementById("table_footer");
+    var tf = document.createElement("th");
+    tf.textContent = "Hourly Totals";
+    var sum = 0;
+    tFooter.appendChild(tf);
+    for(var i=0; i < hours.length; i++){
+        var tf = document.createElement("th");
+        tf.textContent = CookieStore.totalData[i];
+        sum += CookieStore.totalData[i];
+        tFooter.appendChild(tf);
+    }  
+    tf = document.createElement("th");
+    tf.textContent = sum;
+    tFooter.appendChild(tf);
+}
+
+
+
+CookieStore.prototype.render = function (){
+    var cookieTotal = 0;
+    var tbody = document.getElementById("tableData");
+    var tr = document.createElement('tr');
+    tbody.appendChild(tr);
+    /// Get location
+    var tdLocation = document.createElement("td");
+    tdLocation.textContent = this.location;
+    tr.appendChild(tdLocation);
+    //create table data
+    for(var i=0; i<hours.length; i++){
+        var hourlyCookies = this.cookiesSold[i];
+        cookieTotal += hourlyCookies;
+        CookieStore.totalData[i] += hourlyCookies;
+        var cookieHourlyData = document.createElement('td');
+        cookieHourlyData.textContent = hourlyCookies;
+        tr.appendChild(cookieHourlyData);
+      }
+      var totalDailyCookie = document.createElement('td');
+      totalDailyCookie.textContent = cookieTotal;
+      tr.appendChild(totalDailyCookie);
+    };     
+
+
+// function tableData() {
+    
+//     var tbody = document.getElementById("");
+//     var trEL = document.createElement("tr");
+//     for(var i = 0; i < hours.length; i++){
+//         //var tr = document.createElement("tr");
+//         trEL.textContent = this.totalData[i];
+//         tbody.appendChild(trEL);
+//         }  
+     
+//     };
+
+var firstAndPike = new CookieStore("First and Pike",23,65,6.3,);
+var seaTac = new CookieStore("SeaTac Airport",3,24,1.2);
+var seattleCenter = new CookieStore("Seattle Center",11,38,3.7);
+var capitolHill = new CookieStore("Capitol Hill",20,38,2.3);
+var alki =  new CookieStore("Alki",2,16,4.6);
+var cookieTable = [];
+
+headerData();
+// tableData();
+firstAndPike.render();
+seaTac.render();
+seattleCenter.render();
+capitolHill.render();
+alki.render();
+footerData();
